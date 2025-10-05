277888d7c98a91a340bdf49b437104a6d52b75bb
Davi Dias <157860462+DevDaviDias@users.noreply.github.com>
2025-10-05 14:10:35 +0000

    feat:adaptação da API express para vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
+import serverless from 'serverless-http';
 
 const app = express();
-const PORT = 3000;
 
 const rawData = fs.readFileSync('./data.json');
 const data = JSON.parse(rawData);
 
 app.use(cors());
 
+// Rotas
 app.get('/', (req, res) => {
-  res.send(data);
+  res.json(data);
 });
 
 app.get('/personagem', (req, res) => {
@@ -19,14 +20,13 @@ app.get('/personagem', (req, res) => {
 });
 
 app.get('/personagem/:mbti', (req, res) => {
-  const mbti = req.params.mbti.toLocaleUpperCase();
+  const mbti = req.params.mbti.toUpperCase();
   if (data[mbti]) {
     res.json(data[mbti]);
   } else {
-    res.status(404).json({ error: 'tipo MBTI não encontrado' });
+    res.status(404).json({ error: 'Tipo MBTI não encontrado' });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+// Exporta como função serverless
+export const handler = serverless(app);
