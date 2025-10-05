2dc4da40ced286c20718f2531112c92d2d8f5d16
Davi Dias <157860462+DevDaviDias@users.noreply.github.com>
2025-10-05 00:58:45 +0000

    criação de pastas e organização do ambiente

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors"
+import fs from "fs";
+
+const app = express();
+const PORT = 3000;
+
+const rawData = fs.readFileSync("./data.json");
+const data = JSON.parse(rawData);
+
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.send("API School of Elite rodando! Use /personagem ou /personagem/:mbti");
+});
+
+app.get("/personagem",(req,res)=>{
+    res.json(data);
+})
+
+app.get("/personagem/:mbti", (req, res) => {
+    const mbti = req.params.mbti.toLocaleUpperCase();
+    if(data[mbti]){
+        res.json(data[mbti]);
+    }else{
+        res.status(404).json({error:"tipo MBTI não encontrado"});
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
